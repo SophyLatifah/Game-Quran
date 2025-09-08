36ce9bae2df151e3580701094aac705ed6e815bd
Sophy <sophy.latifah.22@gmail.com>
2025-09-08 13:58:51 +0700

    add routing for dashboard, login, signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// import halaman
+import AdminDashboard from "./pages/admin/dashboardadmin";
+import ManageSoal from "./pages/admin/managesoal";
+import UserDashboard from "./pages/user/dashboard";
+import Hijaiyah from "./pages/user/hijaiyah";
+import Statistik from "./pages/user/statistik";
+import Login from "./pages/auth/login";
+import Signup from "./pages/auth/signup";
 
+// import komponen
+import Navbar from "./component/navbar";
+
+function App () {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* AUTH */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup/>} />
+
+        {/* USER */}
+          <Route path="/user" element={<UserDashboard/>} />
+          <Route path="/user/hijaiyah" element={<Hijaiyah/>} />
+          <Route path="/user/statistik" element={<Statistik/>} />
+
+        {/* ADMIN */}
+           <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/soal" element={<ManageSoal />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
