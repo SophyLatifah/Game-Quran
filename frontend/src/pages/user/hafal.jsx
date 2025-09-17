9568306345688c25a1173d5f5fe66271411883d4
Sophy <sophy.latifah.22@gmail.com>
2025-09-17 09:29:47 +0700

    feat : routing dashboard

---
+++ frontend/src/pages/user/hafal.jsx
@@ -0,0 +1,10 @@
+import {React} from "react";
+import { Link } from "react-router-dom";
+
+function Hafal (){
+    return (
+        <h1>halo ini buat hafal nya</h1>
+    );
+};
+
+export default Hafal;
---
+++ frontend/src/pages/user/profil.jsx
@@ -0,0 +1,10 @@
+import {React} from "react";
+import { Link } from "react-router-dom";
+
+function Profil (){
+    return (
+        <h1>halo ini buat profil nya</h1>
+    );
+};
+
+export default Profil;
